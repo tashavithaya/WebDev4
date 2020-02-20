6704e63d73e5fe1814ac793889c7ef7fa97d94fe
tashavithaya <tashavithaya@gmail.com>
2020-02-20 13:53:35 -0800

    added sign up func

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function openForm() {
+    document.getElementsByClassName("signup").style.display = "flex";
+  }
+  
+  function closeForm() {
+    document.getElementsByClassName("signup").style.display = "none";
+  }
